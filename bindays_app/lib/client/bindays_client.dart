62d96e74c11fa65255e8f2422168519538c38c91
BadgerHobbs <andyjriggs@gmail.com>
2025-11-29 12:44:49 +0000

    Update import comments.

--- bindays_app/lib/client/bindays_client.dart
+++ bindays_app/lib/client/bindays_client.dart
@@ -1,6 +1,8 @@
 // External Imports
-import 'package:bindays_app/client/client_with_retry.dart';
 import 'package:bindays_client/client.dart';
+
+// Internal Imports
+import 'package:bindays_app/client/client_with_retry.dart';
 
 ClientWithRetry binDaysClient = ClientWithRetry(
   Client(Uri.parse("https://api.bindays.app")),
